012beaf3f2493a5bd47c12c94e1d9c422cc16181
Валерий <kaktuz34@gmail.com>
2015-10-21 18:16:19 +0300

    upd

--- site/js/common.js
+++ site/js/common.js
@@ -71,4 +71,17 @@ $(document).ready(function() {
 		$(this).parents('.js-inf').remove();
 	});
 
+	// slider init
+	$('.js-slider').slick({
+		dots: true,
+		infinite: true,
+		speed: 1000,
+		fade: true,
+		cssEase: 'linear',
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+	});
+
 });
