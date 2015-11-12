8ea3b0ac3b414cb53df1336dbbb333bd3c834cd3
Валерий <kaktuz34@gmail.com>
2015-11-12 16:39:13 +0200

    contacts page

--- site/js/app.js
+++ site/js/app.js
@@ -267,6 +267,250 @@ $(document).ready(function() {
 		google.maps.event.addDomListener(window, 'load', initialize);
 	};
 
+	// map-contact
+	$('#map-contact').each(function() {
+		function initialize() {
+			var myLatlng = {lat: 53.38297, lng: -1.4659 };
+
+			var map = new google.maps.Map(document.getElementById('map-contact'), {
+				zoom: 15,
+				center 				: myLatlng,
+				mapTypeControl 		: false,
+				streetViewControl 	: false,
+				styles 				: castum,
+				zoomControl 		: true,
+				zoomControlOptions 	: {
+					style: google.maps.ZoomControlStyle.LARGE,
+					position: google.maps.ControlPosition.LEFT_CENTER
+				},
+				panControl: true,
+				panControlOptions 	: {
+					position: google.maps.ControlPosition.LEFT_CENTER
+				}
+			});
+
+			var marker = new google.maps.Marker({
+				position: myLatlng,
+				map 	: map,
+				title: 'Click to zoom',
+				icon: 'img/marker.png'
+			});
+
+			map.addListener('center_changed', function() {
+				window.setTimeout(function() {
+				  map.panTo(marker.getPosition());
+				}, 3000);
+			});
+
+			marker.addListener('click', function() {
+				map.setZoom(8);
+				map.setCenter(marker.getPosition());
+			});
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);
+
+		var castum = [
+			{
+				"featureType": "all",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"saturation": 36
+					},
+					{
+						"color": "#333333"
+					},
+					{
+						"lightness": 40
+					}
+				]
+			},
+			{
+				"featureType": "all",
+				"elementType": "labels.text.stroke",
+				"stylers": [
+					{
+						"visibility": "on"
+					},
+					{
+						"color": "#ffffff"
+					},
+					{
+						"lightness": 16
+					}
+				]
+			},
+			{
+				"featureType": "all",
+				"elementType": "labels.icon",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "administrative",
+				"elementType": "geometry.fill",
+				"stylers": [
+					{
+						"color": "#fefefe"
+					},
+					{
+						"lightness": 20
+					}
+				]
+			},
+			{
+				"featureType": "administrative",
+				"elementType": "geometry.stroke",
+				"stylers": [
+					{
+						"color": "#fefefe"
+					},
+					{
+						"lightness": 17
+					},
+					{
+						"weight": 1.2
+					}
+				]
+			},
+			{
+				"featureType": "landscape",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#f5f5f5"
+					},
+					{
+						"lightness": 20
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#f5f5f5"
+					},
+					{
+						"lightness": 21
+					}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#dedede"
+					},
+					{
+						"lightness": 21
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry.fill",
+				"stylers": [
+					{
+						"color": "#ffffff"
+					},
+					{
+						"lightness": 17
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry.stroke",
+				"stylers": [
+					{
+						"color": "#ffffff"
+					},
+					{
+						"lightness": 29
+					},
+					{
+						"weight": 0.2
+					}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#ffffff"
+					},
+					{
+						"lightness": 18
+					}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#ffffff"
+					},
+					{
+						"lightness": 16
+					}
+				]
+			},
+			{
+				"featureType": "transit",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#f2f2f2"
+					},
+					{
+						"lightness": 19
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#e9e9e9"
+					},
+					{
+						"lightness": 17
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "geometry.fill",
+				"stylers": [
+					{
+						"color": "#cde0fd"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#cde0fd"
+					},
+					{
+						"visibility": "on"
+					}
+				]
+			}
+		]
+	});
+
 	// range
 	$( ".js-slider-range" ).each(function(){
 		var slider = $(this),
